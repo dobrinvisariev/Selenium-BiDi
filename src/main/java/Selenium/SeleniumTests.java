eebeb257b411003f96b5e9762796c580e63e4c07
dobrinvisariev <dobrin.visariev@trivago.com>
2024-01-12 14:19:13 +0100

    JS Console Log test implemented

--- src/main/java/Selenium/SeleniumTests.java
+++ src/main/java/Selenium/SeleniumTests.java
@@ -3,20 +3,22 @@ package Selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.bidi.LogInspector;
 import org.openqa.selenium.bidi.log.ConsoleLogEntry;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.*;
+import java.time.Duration;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class SeleniumTests {
 
     private WebDriver driver;
+    CopyOnWriteArrayList<ConsoleLogEntry> logsList = new CopyOnWriteArrayList<>();
 
     @Before
     public void browser() {
@@ -24,21 +26,35 @@ public class SeleniumTests {
         // webSocketUrl= true enables the Communication between the WebDriver with the browser
         options.setCapability("webSocketUrl", true);
         driver = new ChromeDriver(options);
-        driver.get("https://www.trivago.co.uk");
+
     }
 
     @Test
     public void testJavaScriptConsoleLog() {
-        CopyOnWriteArrayList<ConsoleLogEntry> logs = new CopyOnWriteArrayList<>();
 
         try (LogInspector logInspector = new LogInspector(driver)) {
-            logInspector.onConsoleEntry(logs::add);
-        } catch (Exception e) {
-            // Handle exceptions
+            logInspector.onConsoleEntry(logsList::add);
         }
 
+        driver.get("https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html");
         driver.findElement(By.id("consoleLog")).click();
+        driver.findElement(By.id("consoleError")).click();
+        // wait until the logs list has some entry
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(_d -> !logsList.isEmpty());
+        Assertions.assertEquals("Hello, world!", logsList.get(0).getText());
+        showConsoleLog();
 
+    }
+
+    public void showConsoleLog() {
+
+        if (!logsList.isEmpty()) {
+            for (ConsoleLogEntry log : logsList) {
+                System.out.println(" Console logs: " + log.getText());
+            }
+        } else {
+            System.out.println(" No logs found! ");
+        }
     }
 
     @After
