e01eb5f335dbf8eeb27e8a487bcecccbc1f9bda2
dobrinvisariev <dobrin.visariev@trivago.com>
2024-01-24 16:55:01 +0100

    Mutation DOM test implemented

--- src/main/java/Selenium/DOMTests.java
+++ src/main/java/Selenium/DOMTests.java
@@ -5,21 +5,20 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchShadowRootException;
+import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WindowType;
 import org.openqa.selenium.bidi.browsingcontext.BrowsingContext;
 import org.openqa.selenium.bidi.browsingcontext.BrowsingContextInfo;
 import org.openqa.selenium.bidi.browsingcontext.ReadinessState;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.HasLogEvents;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.devtools.events.CdpEventTypes.domMutation;
 
@@ -54,35 +53,70 @@ public class DOMTests {
         windowContextInfoList = browserWindow.getTree();
 
         System.out.println("Window Context Info List: " + windowContextInfoList.size() + "\n");
-        
+
         BrowsingContextInfo windowInfo = windowContextInfoList.get(0);
         Assertions.assertEquals(0, windowInfo.getChildren().size());
         Assertions.assertEquals(1, windowContextInfoList.size());
         Assertions.assertEquals(windowIdentifier, windowInfo.getId());
         Assertions.assertTrue(windowInfo.getUrl().contains("trivago"));
+    }
 
-
-
+    public WebElement findElementInShadowDOM(By selector) {
+        WebElement shadowRoot = driver.findElement(By.id("usercentrics-root"));
+        SearchContext shadowDom = null;
+        try {
+            shadowDom = shadowRoot.getShadowRoot();
+        } catch (NoSuchShadowRootException e) {
+            System.out.println("No shadow root found");
+        }
+        return shadowDom.findElement(selector);
     }
 
     @Test
-    public void mutationDOMElements() {
+    public void mutationDOMElements() throws InterruptedException {
 
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-        driver.get("https://www.trivago.co.uk");
-        //driver.navigate().to("https://www.trivago.co.uk");
+        driver.navigate().to("https://www.trivago.co.uk/en-GB/lm/hotels-sofia-bulgaria?search=200-15136;dr-20240815-20240816");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         domMutations = new CopyOnWriteArrayList<>();
 
         ((HasLogEvents) driver).onLogEvent(domMutation(e -> domMutations.add(e.getElement())));
 
-        driver.findElement(By.cssSelector("[data-test-id=uc-accept-all-button]")).click();
+        //findElementInShadowDOM(By.cssSelector("[data-testid='uc-accept-all-button']")).click();
+
+        System.out.println("OLD DOM MUTATIONS: " + domMutations.size() + "\n");
+
+        int intervalOfSecondsBetweenTests = 1, maxConsecutiveChecks = 10, minChecks = 3, numberOfNoMutationDetected = 0, checks = 0;
+        int oldDomMutationsSize = domMutations.size(), newDomMutationsSize;
+        int i = 1;
+
+        // Wait for DOM to be stable for trivago webpage
+        while (checks <= maxConsecutiveChecks) {
+            Thread.sleep(intervalOfSecondsBetweenTests * 1000);
+            checks++;
+            newDomMutationsSize = domMutations.size();
+
+            System.out.println(" NEW DOM MUTATIONS: " + newDomMutationsSize + "\n");
+
+            if (oldDomMutationsSize == newDomMutationsSize) {
+                System.out.println("Check " + i + ": DOM might be stable, rechecking again... " + "\n");
+                numberOfNoMutationDetected++;
+                i++;
+            } else {
+                oldDomMutationsSize = newDomMutationsSize;
+                System.out.println("There is a change in the DOM! " + "\n");
+                System.out.println("Reasigning old DOM mutations size with the new mutation size: " + oldDomMutationsSize + "\n");
+            }
+            if (numberOfNoMutationDetected == minChecks) {
+                System.out.println("DOM is stable! " + "\n");
+                break;
+            }
+        }
     }
 
-   @After
+    @After
     public void closeBrowser() throws InterruptedException {
-        Thread.sleep(500);
+        Thread.sleep(100);
         driver.quit();
     }
 
